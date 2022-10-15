78bc04ade2a2787a3d8bf108d6a7af5c09a1af94
Chingis Ochirov <chingis09@gmail.com>
2022-10-15 18:17:15 +0800

    feature: highlights of page links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Navigation() {
+  const [pages] = useState(["About", "Members", "Treasure", "Feed", "Voting"]);
   const router = useRouter();
   const daoId = router.query.dao as string;
 
@@ -10,26 +11,21 @@ export default function Navigation() {
     <nav className="flex justify-between h-[40px] items-center mb-[30px]">
       <div className="text-[33px] text-[#FAFAFB] font-bold">DEMO DAO</div>
       <ul className="flex gap-[30px] h-min text-white font-bold text-lg">
-        <li>
-          <Link href={`/${daoId}/` }>About</Link>
-        </li>
-        <li>
-          <Link href={`/${daoId}/members` }>Members</Link>
-        </li>
-        <li>
-          <Link
-            href={`/${daoId}/treasure`
-            }
-          >
-            Treasure
-          </Link>
-        </li>
-        <li>
-          <Link href={`/${daoId}/feed` }>Feed</Link>
-        </li>
-        <li className="text-white">
-          <Link href={`/${daoId}/voting` }>Voting</Link>
-        </li>
+        {pages.map((pathname, index) => {
+          const link = `/${daoId}${
+            pathname !== "About" ? `/${pathname.toLowerCase()}` : ""
+          }`;
+          return (
+            <li
+              key={index}
+              className={`relative after:absolute after:content-[''] after:-inset-1 after:top-auto after:h-[2px] after:bg-white ${
+                router.asPath === link ? "after:scale-x-100" : "after:scale-x-0"
+              } hover:after:scale-x-100 hover:after:duration-300`}
+            >
+              <Link href={link}>{pathname}</Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
